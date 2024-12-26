f2b23b8b8c1eb8bce1c99947f03670f36869a3df
Muhamad Aji Wibisono <muhamadajiw71@gmail.com>
2024-12-26 07:17:49 +0700

    feat: added simple tests for oracle smart contract

--- blockchain/contracts/Oracle.sol
+++ blockchain/contracts/Oracle.sol
@@ -3,45 +3,42 @@ pragma solidity ^0.8.23;
 
 contract Oracle {
     Request[] requests;
-    uint currentId = 0;
-    uint minQuorum = 2;
-    uint totalOracleCount = 3;
+    uint256 public currentId = 0;
+    uint256 public minQuorum = 2;
+    uint256 public totalOracleCount = 3;
 
     struct Request {
-        uint id;
+        uint256 id;
         string url;
         string attr;
         string value;
-        mapping(uint => string) answers;
+        mapping(uint256 => string) answers;
         mapping(address => uint) quorum;
     }
 
-    event OnNewRequest(uint id, string url, string attr);
-    event OnRequestUpdate(uint id, string url, string attr, string value);
+    event OnNewRequest(uint256 id, string url, string attr);
+    event OnRequestUpdate(uint256 id, string url, string attr, string value);
 
     function createRequest(string memory _url, string memory _attr) public {
-        Request storage newRequest = requests.push();
-        newRequest.id = currentId;
-        newRequest.url = _url;
-        newRequest.attr = _attr;
-        newRequest.value = "";
-
-        newRequest.quorum[address(0x1)] = 1;
-        newRequest.quorum[address(0x2)] = 1;
-        newRequest.quorum[address(0x3)] = 1;
-
+        // Request storage newRequest = requests.push();
+        // newRequest.id = currentId;
+        // newRequest.url = _url;
+        // newRequest.attr = _attr;
+        // newRequest.value = "";
+        // newRequest.quorum[address(0x1)] = 1;
+        // newRequest.quorum[address(0x2)] = 1;
+        // newRequest.quorum[address(0x3)] = 1;
         emit OnNewRequest(currentId, _url, _attr);
-
         currentId++;
     }
 
-    function updateRequest(uint _id, string memory _value) public {
+    function updateRequest(uint256 _id, string memory _value) public {
         Request storage currentRequest = requests[_id];
 
         if (currentRequest.quorum[address(msg.sender)] == 1) {
             currentRequest.quorum[msg.sender] = 2;
 
-            uint iter = 0;
+            uint256 iter = 0;
             bool found = false;
             while (!found) {
                 if (bytes(currentRequest.answers[iter]).length == 0) {
@@ -51,9 +48,9 @@ contract Oracle {
                 iter++;
             }
 
-            uint currentQuorum = 0;
+            uint256 currentQuorum = 0;
 
-            for (uint i = 0; i < totalOracleCount; i++) {
+            for (uint256 i = 0; i < totalOracleCount; i++) {
                 bytes memory a = bytes(currentRequest.answers[i]);
                 bytes memory b = bytes(_value);
 
---
+++ blockchain/test/Oracle.ts
@@ -0,0 +1,57 @@
+import { HardhatEthersSigner } from "@nomicfoundation/hardhat-ethers/signers"
+import { expect } from "chai"
+import { ethers } from "hardhat"
+import { Oracle } from "../typechain-types/contracts/Oracle"
+
+describe("Oracle", () => {
+    let oracleContract: Oracle
+    let signers: HardhatEthersSigner[]
+    const url = "http://localhost:3000"
+    const attr = "test"
+
+    beforeEach(async function () {
+        signers = await ethers.getSigners()
+        const oracleContractFactory = await ethers.getContractFactory("Oracle")
+        oracleContract = await oracleContractFactory.deploy()
+        await oracleContract.waitForDeployment()
+    })
+
+    describe("# Base test", function () {
+        it("should check the validity of variables", async function () {
+            const minQuorum = await oracleContract.minQuorum()
+            const totalOracleCount = await oracleContract.totalOracleCount()
+            expect(totalOracleCount).to.greaterThanOrEqual(
+                minQuorum,
+                `Expected totalOracleCount (${totalOracleCount}) to be greater than or equal to minQuorum (${minQuorum})`
+            )
+        })
+    })
+
+    describe("# CreateRequest", function () {
+        it("should create a new request", async function () {
+            await oracleContract.createRequest(url, attr)
+
+            const filter = oracleContract.filters.OnNewRequest
+            const events = await oracleContract.queryFilter(filter)
+            const event = events[0]
+
+            console.log(event)
+
+            expect(event).to.not.be.undefined
+            expect(event.fragment.name).to.equal("OnNewRequest")
+            expect(event?.args?.id).to.equal(0)
+            expect(event?.args?.url).to.equal(url)
+            expect(event?.args?.attr).to.equal(attr)
+        })
+    })
+
+    describe("# updateRequest", function () {
+        // beforeEach(async function (params) {
+        //     await oracle.createRequest(url, attr)
+        // })
+        // it("should update request when quorum is reached", async function () {
+        //     const value = "100"
+        //     await oracle.connect(addr1).updateRequest(0, oracleValue)
+        // })
+    })
+})
