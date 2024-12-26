df1ce55e0bc5ff6e96c5995d765051b7dedc3fc6
Muhamad Aji Wibisono <muhamadajiw71@gmail.com>
2024-12-26 09:23:49 +0700

    feat: db apis

--- blockchain/oracle/db.ts
+++ blockchain/oracle/db.ts
@@ -1,70 +1,83 @@
 import sqlite3 from "sqlite3"
 import { Database, open } from "sqlite"
 
-let dbInstance: Database | null = null
-
-export async function getDB(): Promise<Database> {
-    if (!dbInstance) {
-        dbInstance = await open({
-            filename: "oracle.db",
-            driver: sqlite3.Database
-        })
+export class Db {
+    static instance: Database | null = null
+    static async getInstance(): Promise<Database> {
+        if (!Db.instance) {
+            Db.instance = await open({
+                filename: "oracle.db",
+                driver: sqlite3.Database
+            })
+        }
+        return Db.instance
     }
-    return dbInstance
-}
 
-export async function initDB(): Promise<void> {
-    const db = await getDB()
+    async Init(): Promise<void> {
+        const dbInstance = await Db.getInstance()
 
-    try {
-        await db.run(`
-            CREATE TABLE IF NOT EXISTS voters (
-                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                voter_id TEXT NOT NULL,
-                tps_id INTEGER NOT NULL,
-                voting_id INTEGER NOT NULL
+        try {
+            await dbInstance.run(`
+                CREATE TABLE IF NOT EXISTS voters (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT,
+                    voter_id TEXT NOT NULL,
+                    tps_id INTEGER NOT NULL,
+                    voting_id INTEGER NOT NULL
+                )
+            `)
+            console.log("Database initialized")
+        } catch (error) {
+            console.log("Database failed to initialize: ", error)
+            throw error
+        }
+    }
+
+    async InsertVoter(voter_id: string, tps_id: number, voting_id: number): Promise<void> {
+        const dbInstance = await Db.getInstance()
+
+        try {
+            await dbInstance.run(
+                `
+                INSERT INTO voters (voter_id, tps_id, voting_id) VALUES (?, ?, ?)
+            `,
+                voter_id,
+                tps_id,
+                voting_id
             )
-        `)
-        console.log("Database initialized")
-    } catch (error) {
-        console.log("Database failed to initialize: ", error)
-        throw error
+        } catch (error) {
+            console.log("Failed to insert voter: ", error)
+            throw error
+        }
     }
-}
 
-export async function InsertVoter(voter_id: string, tps_id: number, voting_id: number): Promise<void> {
-    const db = await getDB()
+    async GetVoters(): Promise<boolean> {
+        const dbInstance = await Db.getInstance()
 
-    try {
-        await db.run(
-            `
-            INSERT INTO voters (voter_id, tps_id, voting_id) VALUES (?, ?, ?)
-        `,
-            voter_id,
-            tps_id,
-            voting_id
-        )
-    } catch (error) {
-        console.log("Failed to insert voter: ", error)
-        throw error
+        try {
+            const result = await dbInstance.get(`SELECT * FROM voters`)
+            return result
+        } catch (error) {
+            console.log("Failed to fetch voter data: ", error)
+            throw error
+        }
     }
-}
 
-export async function IsVoterRegistered(voter_id: string, tps_id: number, voting_id: number): Promise<boolean> {
-    const db = await getDB()
+    async IsVoterRegistered(voter_id: string, tps_id: number, voting_id: number): Promise<boolean> {
+        const dbInstance = await Db.getInstance()
 
-    try {
-        const result = await db.get(
-            `
-            SELECT * FROM voters WHERE voter_id = ? AND tps_id = ? AND voting_id = ?
-        `,
-            voter_id,
-            tps_id,
-            voting_id
-        )
-        return !!result
-    } catch (error) {
-        console.log("Failed to check voter registration: ", error)
-        throw error
+        try {
+            const result = await dbInstance.get(
+                `
+                SELECT * FROM voters WHERE voter_id = ? AND tps_id = ? AND voting_id = ?
+            `,
+                voter_id,
+                tps_id,
+                voting_id
+            )
+            return !!result
+        } catch (error) {
+            console.log("Failed to check voter registration: ", error)
+            throw error
+        }
     }
 }
--- blockchain/oracle/server.ts
+++ blockchain/oracle/server.ts
@@ -1,16 +1,77 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from "@hono/node-server"
+import { Hono } from "hono"
+import { Db } from "./db"
 
+//--------------------Constants--------------------
+const port = 3000
 const app = new Hono()
+const db = new Db()
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+//--------------------Routes--------------------
+app.get("/", (c) => {
+    return c.text("Oracle server is running")
+})
+app.get("/voters", async (c) => {
+    try {
+        const result = await db.GetVoters()
+        return c.json({ result })
+    } catch (error) {
+        return c.json({ error: error.message }, 500)
+    }
+})
+app.post("/voters", async (c) => {
+    const body = await c.req.json()
+    const b_voter_id = body.voter_id
+    const b_tps_id = body.tps_id
+    const b_voting_id = body.voting_id
+
+    if (!b_voter_id || !b_tps_id || !b_voting_id) {
+        return c.json({ error: "Invalid parameters" }, 400)
+    }
+
+    const voter_id = b_voter_id as string
+    const tps_id = parseInt(b_tps_id as string)
+    const voting_id = parseInt(b_voting_id as string)
+
+    if (isNaN(tps_id) || isNaN(voting_id)) {
+        return c.json({ error: "Invalid parameters" }, 400)
+    }
+
+    try {
+        await db.InsertVoter(voter_id, tps_id, voting_id)
+        return c.json({ success: true })
+    } catch (error) {
+        return c.json({ error: error.message }, 500)
+    }
+})
+app.get("/voters/validity?voter_id=:voter_id&tps_id=:tps_id&voting_id=:voting_id", async (c) => {
+    const p_voter_id = c.req.param("voter_id")
+    const p_tps_id = c.req.param("tps_id")
+    const p_voting_id = c.req.param("voting_id")
+
+    if (!p_voter_id || !p_tps_id || !p_voting_id) {
+        return c.json({ error: "Invalid parameters" }, 400)
+    }
+
+    const voter_id = p_voter_id as string
+    const tps_id = parseInt(p_tps_id as string)
+    const voting_id = parseInt(p_voting_id as string)
+
+    if (isNaN(tps_id) || isNaN(voting_id)) {
+        return c.json({ error: "Invalid parameters" }, 400)
+    }
+
+    try {
+        const result = await db.IsVoterRegistered(voter_id, tps_id, voting_id)
+        return c.json({ result })
+    } catch (error) {
+        return c.json({ error: error.message }, 500)
+    }
 })
 
-const port = 3000
+//--------------------Main--------------------
 console.log(`Server is running on http://localhost:${port}`)
-
 serve({
-  fetch: app.fetch,
-  port
+    fetch: app.fetch,
+    port
 })
