9e1820d082d4ffb7609a16e3f04d2bd1a6889962
Akbar Maulana Ridho <akbarmaulanaridho1@gmail.com>
2024-12-26 17:35:49 +0700

    update deploy

--- blockchain/tasks/deploy.ts
+++ blockchain/tasks/deploy.ts
@@ -1,4 +1,23 @@
 import { task } from "hardhat/config"
+import { ISemaphore } from "../typechain-types"
+
+const testData = {
+    votings: [
+        {
+            id: 1, // 1 for ID pilgub Jawa Barat
+            candidateCount: 3
+        },
+        {
+            id: 5, // 5 for ID piwalkot Kota Bandung
+            candidateCount: 2
+        }
+    ],
+    tpsIds: [10, 20, 30],
+    registerStartAt: new Date("2024-12-24"),
+    registerEndAt: new Date("2025-12-25"),
+    voteStartAt: new Date("2024-12-24"),
+    voteEndAt: new Date("2025-12-25")
+}
 
 export interface DeployVotingContractArgs {
     semaphore: string
@@ -11,7 +30,7 @@ export interface DeployVotingContractArgs {
     voteEndAt: Date
 }
 
-task<DeployVotingContractArgs>("deploy", "Deploy a voting contract").setAction(async (args, { ethers }) => {
+task<DeployVotingContractArgs>("deploy", "Deploy a voting contract", async (args, { ethers }) => {
     const ElectionFactory = await ethers.getContractFactory("Election")
 
     const electionContract = await ElectionFactory.deploy(
@@ -30,4 +49,27 @@ task<DeployVotingContractArgs>("deploy", "Deploy a voting contract").setAction(a
     )
 
     return electionContract
+})
+
+task("deployTest", "Deploy an election scenario", async (args, { run }) => {
+    const { semaphore } = await run("deploy:semaphore", {
+        logs: false
+    })
+
+    const semaphoreContract: ISemaphore = semaphore
+
+    for (const voting of testData.votings) {
+        for (const tps of testData.tpsIds) {
+            await run("deploy", {
+                semaphore: await semaphoreContract.getAddress(),
+                pollingStationId: tps,
+                votingId: voting.id,
+                candidateCount: voting.candidateCount,
+                registerStartAt: testData.registerStartAt,
+                registerEndAt: testData.registerEndAt,
+                voteStartAt: testData.voteStartAt,
+                voteEndAt: testData.voteEndAt
+            })
+        }
+    }
 })
